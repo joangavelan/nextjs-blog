df8d3f30377d6102ce5f69efc6dfa7f90d087863
Joan Gavelán <joangavelan@gmail.com>
2022-05-09 12:48:49 -0500

    app converted into typescript

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,28 +1,32 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head';
-import Date from '../../components/date';
-import utilStyles from '../../styles/utils.module.css';
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds()
-
   return {
     paths,
     fallback: false
   }
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id)
   return {
     props: {
-      postData,
-    },
-  };
+      postData
+    }
+  }
 }
 
-export default function Post({ postData }) {
+export default function Post({
+  postData
+}: {
+  postData: { title: string; date: string; contentHtml: string }
+}) {
   return (
     <Layout>
       <Head>
@@ -36,5 +40,5 @@ export default function Post({ postData }) {
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
-  );
+  )
 }
